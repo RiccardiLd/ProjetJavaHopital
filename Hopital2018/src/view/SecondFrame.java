77cae4152dd53d7283eb086367cb3b37c114c3ec
Reinim <minier.theo@gmail.com>
2018-04-20 19:16:10 +0200

    creation du string pour add

--- Hopital2018/src/view/SecondFrame.java
+++ Hopital2018/src/view/SecondFrame.java
@@ -18,19 +18,24 @@ public class SecondFrame extends JFrame{
     
     private final Controller controleur;
     int nbColonnes;
+    javax.swing.JTextField[] texte;
+    javax.swing.JLabel[] label;
     
     
     public SecondFrame(Controller controleur) throws SQLException {
         this.controleur = controleur;
         nbColonnes = controleur.maConnexion.rsetMeta.getColumnCount();
+        texte = new javax.swing.JTextField[nbColonnes];
+        label = new javax.swing.JLabel[nbColonnes];
         init();
+        createAddRequete();
     }
     
     private void init() throws SQLException {
         JPanel pane = new JPanel();
         
-        javax.swing.JTextField[] texte = new javax.swing.JTextField[nbColonnes];
-        javax.swing.JLabel[] label = new javax.swing.JLabel[nbColonnes];
+        
+        
          for(int i = 1; i <= nbColonnes; i++)
 
         {
@@ -45,4 +50,51 @@ public class SecondFrame extends JFrame{
         this.setVisible(true);
     }
     
+     private String createAddRequete() throws SQLException
+{
+    /*
+INSERT INTO T_TITRE (TIT_CODE, TIT_LIBELLE)
+       VALUES  ('M.'    , 'Monsieur'),
+                ('Mlle.' , 'Mademoiselle'),
+                ('Mme.'  , 'Madame');*/
+    
+       String requete = "INSERT INTO " + controleur.maConnexion.rsetMeta.getTableName(1) + " (";
+       for(int i =0; i<nbColonnes; i++)
+       {
+          if(verifTexteExiste(label[i].getText().trim()))
+           {
+           requete += label[i].getText().trim();
+           if(nbColonnes>i+1)
+           requete += ", ";
+           }
+       }
+       
+       requete += ") VALUES ( ";
+        
+       for(int i =0; i<nbColonnes; i++)
+       {
+           if(verifTexteExiste(texte[i].getText().trim()))
+           {
+            requete += texte[i].getText().trim();
+           if(nbColonnes>i+1)
+           requete += ", ";
+           }
+           
+       }
+       requete += ")";
+       
+       System.out.println(requete);
+    
+    return requete;
 }
+    private boolean verifTexteExiste(String s){
+        
+        if(s!= null && s!= "")
+            return true;
+        
+        
+        return false;
+    }
+        
+}
+   
